f5c3b3cf0eb3ccfc8d281d15b8209a3fb2f6d5e5
Jake Dudum <jakedudum@gmail.com>
2020-10-11 17:29:41 -0700

    Finished practical-app section 1.

--- _practical-app/1.php
+++ _practical-app/1.php
@@ -19,7 +19,7 @@
 
 
 		/* 
-		   Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
+		   Step 1:  Use the Echo Function to say hello with html h1 tags embedded inside php.
 
 
 		   Step 2: Write a comment above the echo function and explain
@@ -27,6 +27,9 @@
 
 
 		   */
+
+		   // Prints hello to the DOM
+		   echo "<h1>Hello</h1>";
 
 		?>
 
