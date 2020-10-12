7b097fdad86bcf7be7a7524c64ff3003a6120d4d
Jake Dudum <jakedudum@gmail.com>
2020-10-11 17:25:07 -0700

    Initialized project with start Udemy files.

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php echo "HELLO WORLD"; ?>
+
+<div class="demo">
+    <ul>
+        <li><a href="">home</a></li>
+        <li><a href="">home</a></li>
+        <li><a href="">home</a></li>
+        <li><a href="">home</a></li>
+        <li><a href="">home</a></li>
+    </ul>
+</div>
+
