42108ee2b9c61285093c2ba2c17c4604a7119315
Jake Dudum <jakedudum@gmail.com>
2020-10-14 19:12:11 -0700

    Finished practice section 5.

--- _practical-app/5.php
+++ _practical-app/5.php
@@ -12,19 +12,22 @@
 <article class="main-content col-xs-8">
 
 	
-	<?php 
+<?php 
 
 
 /*  Step1: Use a pre-built math function here and echo it
 
-
 	Step 2:  Use a pre-built string function here and echo it
-
 
 	Step 3:  Use a pre-built Array function here and echo it
 
  */
 
+echo floor(3.14159265359) . "<br>";
+
+echo strlen("How long is this string?") . "<br>";
+
+echo min([23423,12412,457456,1234312,3456,234523,456,234,346789]);
 	
 ?>
 
